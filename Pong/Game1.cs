b6cba16d0673b56ce790f6fcb465ff7803527a05
diduk <diduk.mail@gmail.com>
2026-04-23 11:45:41 +0200

    Első feladat: A "világ" létrehozásaA dokumentumod alapján már tudod, hogyan kell textúrát betölteni.
    feladatod:Deklarálj 3 Rectangle változót: leftPaddle, rightPaddle, és ball.
    A LoadContent-ben hozz létre egy egyszerű fehér textúrát (ezt nevezzük "pixel"-nek), amit mindhárom elem kirajzolásához használni fogsz.
    A Draw metódusban rajzold ki ezt a három téglalapot a megfelelő helyre.
    
    Tanári segítség a fehér pixelez:
    pixel = new Texture2D(GraphicsDevice, 1, 1);pixel.SetData(new[] { Color.White }); Így a Draw-nál a sourceRectangle helyett a cél-téglalapot (ball, leftPaddle stb.) adod meg, és a MonoGame akkorára nyújtja a fehér pixelt, amekkorára akarod.

--- Pong/Game1.cs
+++ Pong/Game1.cs
@@ -1,6 +1,24 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+/*
+ Első feladat: A "világ" létrehozásaA dokumentumod alapján már tudod, hogyan kell textúrát betölteni.
+
+feladatod:Deklarálj 3 Rectangle változót: leftPaddle, rightPaddle, és ball.
+A LoadContent-ben hozz létre egy egyszerű fehér textúrát (ezt nevezzük "pixel"-nek), amit mindhárom elem kirajzolásához használni fogsz.
+A Draw metódusban rajzold ki ezt a három téglalapot a megfelelő helyre.
+
+Tanári segítség a fehér pixelez:
+pixel = new Texture2D(GraphicsDevice, 1, 1);pixel.SetData(new[] { Color.White }); Így a Draw-nál a sourceRectangle helyett a cél-téglalapot (ball, leftPaddle stb.) adod meg, és a MonoGame akkorára nyújtja a fehér pixelt, amekkorára akarod.
+
+ színpad : 800 x 480
+
+// A jobb szél lekérdezése:
+int jobbSzel = GraphicsDevice.Viewport.Width; 
+
+// Az alja lekérdezése:
+int ablakAlja = GraphicsDevice.Viewport.Height;
+ */
 
 namespace Pong
 {
@@ -8,6 +26,12 @@ namespace Pong
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        Texture2D pixel;
+        Rectangle leftPaddle;
+        Rectangle rightPaddle;
+        Rectangle ball;
+
 
         public Game1()
         {
@@ -19,6 +43,12 @@ namespace Pong
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            pixel = new Texture2D(GraphicsDevice, 1, 1);
+            pixel.SetData(new[] { Color.White });
+
+            leftPaddle = new Rectangle(5, 240-50, 20, 100);
+            rightPaddle = new Rectangle(800-25, 240-50, 20, 100);
+            ball = new Rectangle(400-8, 240-8, 16, 16);
 
             base.Initialize();
         }
@@ -42,7 +72,16 @@ namespace Pong
 
         protected override void Draw(GameTime gameTime)
         {
+           
             GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            //_spriteBatch.Draw(leftPaddle, new Vector2(100,100), Color.White);
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(pixel, leftPaddle, Color.White);
+            _spriteBatch.Draw(pixel, rightPaddle, Color.White);
+            _spriteBatch.Draw(pixel, ball, Color.White);
+
+            _spriteBatch.End();
 
             // TODO: Add your drawing code here
 
